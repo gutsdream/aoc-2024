2677ee79b67e5891ff23553f1c9aa5a214f1f548
gutsdream <angus.hankinson@gmail.com>
2024-12-02 21:56:12 +1300

    Small readability change

--- day_01/src/lib.rs
+++ day_01/src/lib.rs
@@ -1,19 +1,23 @@
 pub fn get_location_id_pairs(pairs: Vec<&str>) -> (Vec<i32>, Vec<i32>) {
     let (mut left, mut right) : (Vec<i32>, Vec<i32>) = pairs.into_iter().filter_map(|x| {
-        let pair: Vec::<i32> = x.split(' ')
-            .filter_map(|y| y.parse::<i32>().ok())
-            .collect();
-
-        match pair.len() == 2 {
-            true => Some((pair[0], pair[1])),
-            false => None
-        }
+        extract_numeric_pair(x)
     }).collect();
 
     left.sort();
     right.sort();
 
     (left, right)
+}
+
+fn extract_numeric_pair(x: &str) -> Option<(i32, i32)> {
+    let pair: Vec::<i32> = x.split(' ')
+        .filter_map(|y| y.parse::<i32>().ok())
+        .collect();
+
+    match pair.len() == 2 {
+        true => Some((pair[0], pair[1])),
+        false => None
+    }
 }
 
 #[cfg(test)]
