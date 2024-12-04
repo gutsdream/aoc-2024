719f91cb8193ace75a17d3f792295e562df56cc5
gutsdream <angus.hankinson@gmail.com>
2024-12-05 10:54:54 +1300

    Moving part 1 shared logic into lib

--- day_04/src/bin/part_1.rs
+++ day_04/src/bin/part_1.rs
@@ -1,4 +1,4 @@
-use crate::Direction::{East, North, NorthEast, NorthWest, South, SouthEast, SouthWest, West};
+use day_04::{search, DIRECTIONS};
 use eyre::ContextCompat;
 use std::fs;
 
@@ -12,36 +12,6 @@ fn main() -> eyre::Result<()> {
     Ok(())
 }
 
-#[derive(Debug)]
-enum Direction {
-    North,
-    NorthEast,
-    East,
-    SouthEast,
-    South,
-    SouthWest,
-    West,
-    NorthWest,
-}
-
-const DIRECTIONS: [Direction; 8] = [
-    North, NorthEast, East, SouthEast, South, SouthWest, West, NorthWest,
-];
-
-impl Direction {
-    fn traversal(&self) -> (i32, i32) {
-        match self {
-            North => (0, -1),
-            NorthEast => (1, -1),
-            East => (1, 0),
-            SouthEast => (1, 1),
-            South => (0, 1),
-            SouthWest => (-1, 1),
-            West => (-1, 0),
-            NorthWest => (-1, -1),
-        }
-    }
-}
 
 fn solve(input: String) -> eyre::Result<i32> {
     let lines: Vec<String> = input.lines().map(|x| x.to_string()).collect::<Vec<_>>();
@@ -64,25 +34,6 @@ fn solve(input: String) -> eyre::Result<i32> {
     }
 
     Ok(count)
-}
-
-fn search(x: usize, y: usize, lines: &Vec<String>, direction: &Direction, depth: i32) -> String{
-    let word = (0..depth)
-        .enumerate()
-        .filter_map(|(i, _)| {
-            let i = i + 1;
-            let traversal = direction.traversal();
-            let next_y : usize = (y as i32 + (traversal.1 * i as i32)).try_into().ok()?;
-            let next_x : usize = (x as i32 + (traversal.0 * i as i32)).try_into().ok()?;
-
-            let next_line = lines.iter().nth(next_y)?;
-            let next_char = next_line.chars().nth(next_x)?;
-
-            Some(next_char)
-        })
-        .collect::<String>();
-
-    word
 }
 #[cfg(test)]
 mod tests {
---
+++ day_04/src/lib.rs
@@ -0,0 +1,51 @@
+use crate::Direction::{East, North, NorthEast, NorthWest, South, SouthEast, SouthWest, West};
+
+#[derive(Debug)]
+pub enum Direction {
+    North,
+    NorthEast,
+    East,
+    SouthEast,
+    South,
+    SouthWest,
+    West,
+    NorthWest,
+}
+
+pub const DIRECTIONS: [Direction; 8] = [
+    North, NorthEast, East, SouthEast, South, SouthWest, West, NorthWest,
+];
+
+impl Direction {
+    pub fn traversal(&self) -> (i32, i32) {
+        match self {
+            North => (0, -1),
+            NorthEast => (1, -1),
+            East => (1, 0),
+            SouthEast => (1, 1),
+            South => (0, 1),
+            SouthWest => (-1, 1),
+            West => (-1, 0),
+            NorthWest => (-1, -1),
+        }
+    }
+}
+
+pub fn search(x: usize, y: usize, lines: &Vec<String>, direction: &Direction, depth: i32) -> String{
+    let word = (0..depth)
+        .enumerate()
+        .filter_map(|(i, _)| {
+            let i = i + 1;
+            let traversal = direction.traversal();
+            let next_y : usize = (y as i32 + (traversal.1 * i as i32)).try_into().ok()?;
+            let next_x : usize = (x as i32 + (traversal.0 * i as i32)).try_into().ok()?;
+
+            let next_line = lines.iter().nth(next_y)?;
+            let next_char = next_line.chars().nth(next_x)?;
+
+            Some(next_char)
+        })
+        .collect::<String>();
+
+    word
+}
