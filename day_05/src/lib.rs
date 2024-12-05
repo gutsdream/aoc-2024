58561a17b29906184fdd07e245b31febb2f63e01
gutsdream <angus.hankinson@gmail.com>
2024-12-05 20:38:39 +1300

    Small changes, formatting

--- day_05/src/lib.rs
+++ day_05/src/lib.rs
@@ -1,6 +1,6 @@
-pub struct Puzzle{
+pub struct Puzzle {
     rules: Vec<Vec<usize>>,
-    updates: Vec<Vec<usize>>
+    updates: Vec<Vec<usize>>,
 }
 
 impl From<&str> for Puzzle {
@@ -10,102 +10,108 @@ impl From<&str> for Puzzle {
             .filter(|&x| x.len() > 2)
             .partition(|&x| x.contains('|'));
 
-        let rules = rules.into_iter()
-            .filter_map(|x|Some(x.split('|')
-                    .filter_map(|x| x.parse::<usize>().ok())
-                    .collect())
-            ).collect();
-
-        let updates = updates.into_iter()
-            .map(|x| x.split(',')
-                .filter_map(|x| x.parse::<usize>().ok())
-                .collect())
+        let rules = rules
+            .into_iter()
+            .filter_map(|x| {
+                Some(
+                    x.split('|')
+                        .filter_map(|x| x.parse::<usize>().ok())
+                        .collect(),
+                )
+            })
             .collect();
 
-        Puzzle{
-            rules,
-            updates
-        }
+        let updates = updates
+            .into_iter()
+            .map(|x| {
+                x.split(',')
+                    .filter_map(|x| x.parse::<usize>().ok())
+                    .collect()
+            })
+            .collect();
+
+        Puzzle { rules, updates }
     }
 }
 
-impl Puzzle{
-    pub fn sum_of_correct_updates(&self) -> usize{
-        self.updates.iter()
+impl Puzzle {
+    pub fn sum_of_correct_updates(&self) -> usize {
+        self.updates
+            .iter()
             .filter(|x| self.is_valid(x))
-            .map(|x| Puzzle::get_middle_page(x))
+            .map(|x| Self::get_middle_page(x))
             .sum()
     }
 
-    pub fn sum_of_incorrect_updates(&self) -> usize{
-        self.updates.iter()
+    pub fn sum_of_incorrect_updates(&self) -> usize {
+        self.updates
+            .iter()
             .filter(|x| !self.is_valid(x))
             .map(|x| self.correct_update(x.clone()))
-            .map(|x| Puzzle::get_middle_page(&x))
+            .map(|x| Self::get_middle_page(&x))
             .sum()
     }
 
-    // fn get_incorrect_updates(&self) -> Vec<Vec<usize>>{
-    //     self.updates.iter()
-    //         .filter(|x| !self.is_valid(x))
-    //         .collect()
-    // }
-
-    fn get_middle_page(update: &Vec<usize>) -> usize{
+    fn get_middle_page(update: &Vec<usize>) -> usize {
         let len = update.len();
-        let index = match len % 2{
+        let index = match len % 2 {
             0 => len / 2 - 1,
-            _ => len / 2
+            _ => len / 2,
         };
 
         update[index]
     }
 
-    fn is_valid(&self, update: &Vec<usize>) -> bool{
-        let mut avoided_pages: Vec<usize> = vec![];
-        for n in (0..update.len()).rev(){
-            if avoided_pages.contains(&update[n]){
-                return false;
-            }
+    fn is_valid(&self, update: &Vec<usize>) -> bool {
+        let applicable_rules: Vec<&Vec<usize>> = self.applicable_rules(&update);
 
-            let mut avoid_for_update: Vec<usize> = self.rules.iter()
-                .filter(|x| &x[0] == &update[n])
-                .map(|x| x[1])
-                .collect();
-
-            avoided_pages.append(&mut avoid_for_update);
-        }
-
-        true
+        applicable_rules
+            .iter()
+            .all(|x| Self::is_rule_met(&update, x))
     }
 
-    pub fn correct_update(&self, mut update: Vec<usize>) -> Vec<usize>{
-        let applicable_rules : Vec<&Vec<usize>> = self.rules.iter()
-            .filter(|rule| update.contains(&rule[0]))
-            .filter(|rule| update.contains(&rule[1]))
-            .collect();
+    pub fn correct_update(&self, mut update: Vec<usize>) -> Vec<usize> {
+        let applicable_rules: Vec<&Vec<usize>> = self.applicable_rules(&update);
 
         match applicable_rules
             .into_iter()
-            .find(|rule| update.iter().position(|page| page == &rule[0]) > update.iter().position(|page| page == &rule[1])) {
-            None => {update}
+            .find(|rule| !Self::is_rule_met(&mut update, &rule))
+        {
+            None => update,
             Some(broken_rule) => {
-                let left = update.iter().position(|page| page == &broken_rule[0]).unwrap();
-                let right = update.iter().position(|page| page == &broken_rule[1]).unwrap();
+                let left = update
+                    .iter()
+                    .position(|page| page == &broken_rule[0])
+                    .unwrap();
+                let right = update
+                    .iter()
+                    .position(|page| page == &broken_rule[1])
+                    .unwrap();
                 update.swap(left, right);
 
                 self.correct_update(update)
             }
         }
+    }
 
+    fn applicable_rules(&self, update: &Vec<usize>) -> Vec<&Vec<usize>> {
+        self.rules
+            .iter()
+            .filter(|rule| update.contains(&rule[0]))
+            .filter(|rule| update.contains(&rule[1]))
+            .collect()
+    }
 
+    fn is_rule_met(update: &Vec<usize>, rule: &Vec<usize>) -> bool {
+        update.iter().position(|page| page == &rule[0])
+            < update.iter().position(|page| page == &rule[1])
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    const INPUT : &str = "47|53
+    const INPUT: &str = "47|53
 97|13
 97|61
 97|47
