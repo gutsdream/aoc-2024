f918e4f4c10fd9e272fa2fef161d533c9c0ac9ed
gutsdream <angus.hankinson@gmail.com>
2024-12-10 19:07:21 +1300

    Day 10 Part 1

---
+++ day_10/src/lib.rs
@@ -0,0 +1,96 @@
+use direction::Direction;
+use navigation::Map;
+use std::str::FromStr;
+use crate::navigation::{MapPosition, Navigate, Point};
+
+mod direction;
+mod navigation;
+
+pub struct Puzzle {
+    map: Map,
+}
+
+impl FromStr for Puzzle {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let map: Vec<Vec<u32>> = input
+            .lines()
+            .map(|line| line.chars()
+                .filter_map(|c| c.to_digit(10))
+                .collect::<Vec<_>>()
+            )
+            .collect();
+
+        Ok(Puzzle { map })
+    }
+}
+
+impl Puzzle {
+    pub fn part_1(&self) -> u32 {
+        let starting_positions = self.map.iter()
+            .enumerate()
+            .map(|(y, row)| row.iter()
+                .enumerate()
+                .filter_map(|(x, height)| match height == &0 {
+                    true => {Some(MapPosition{
+                        point: Point { x, y },
+                        height: height.clone()
+                    })}
+                    false => {None}
+                }).collect::<Vec<_>>())
+            .flatten()
+            .collect::<Vec<_>>();
+
+        starting_positions
+            .iter()
+            .map(|position| self.map.get_trailhead_score(position.clone()))
+            .sum()
+    }
+
+    pub fn part_2(&self) -> u32 {
+        1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const INPUT: &str = "89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732";
+
+    const SMALL :&str = "0123
+1234
+8765
+9876";
+
+    #[test]
+    fn should_solve_part_1() {
+        // Given
+        let puzzle = Puzzle::from_str(INPUT).unwrap();
+
+        // When
+        let sum = puzzle.part_1();
+
+        // Then
+        assert_eq!(36, sum);
+    }
+
+    #[test]
+    fn should_solve_part_2() {
+        // Given
+        let puzzle = Puzzle::from_str(INPUT).unwrap();
+
+        // When
+        let sum = puzzle.part_2();
+
+        // Then
+        assert_eq!(1, sum);
+    }
+}
---
+++ day_10/src/navigation.rs
@@ -0,0 +1,62 @@
+use itertools::Itertools;
+use crate::direction::{Direction, DIRECTIONS};
+
+pub type Map = Vec<Vec<u32>>;
+
+const START: usize = 0;
+const END: usize = 9;
+
+pub trait Navigate {
+    fn get_trailhead_score(&self, position: MapPosition) -> u32;
+    fn get_next_trail_steps(&self, position: &MapPosition) -> Vec<MapPosition>;
+    fn at_point(&self, point: Point) -> Option<MapPosition>;
+}
+
+impl Navigate for Map {
+    fn get_trailhead_score(&self, position: MapPosition) -> u32 {
+        let final_positions = (0..9).fold(vec![position], |positions, _| {
+            positions.iter()
+                .map(|position| self.get_next_trail_steps(position))
+                .flatten()
+                .collect()
+        });
+
+        final_positions.iter().unique().count() as u32
+    }
+
+    fn get_next_trail_steps(&self, position: &MapPosition) -> Vec<MapPosition> {
+        DIRECTIONS.iter()
+            .filter_map(|direction| position.point.gen_in_direction(direction))
+            .filter_map(|point| self.at_point(point))
+            .filter(|next_position| next_position.height.checked_sub(position.height) == Some(1))
+            .collect()
+    }
+
+    fn at_point(&self, point: Point) -> Option<MapPosition> {
+        self.get(point.y)
+            .and_then(|row| row.get(point.x))
+            .and_then(|height| Some(MapPosition{ point, height: height.clone() }))
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct Point {
+    pub x: usize,
+    pub y: usize,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct MapPosition {
+    pub point: Point,
+    pub height: u32,
+}
+
+impl Point {
+    fn gen_in_direction(&self, direction: &Direction) -> Option<Self> {
+        let traversal = direction.traversal();
+        let x: usize = (self.x as isize + traversal.0).try_into().ok()?;
+        let y: usize = (self.y as isize + traversal.1).try_into().ok()?;
+
+        Some(Point { x, y })
+    }
+}
