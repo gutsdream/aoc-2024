1f7b091eaf7893450c935c91f2d0c0764c9bceba
gutsdream <angus.hankinson@gmail.com>
2024-12-14 11:37:45 +1300

    Day 13 - bruteforced part 2

--- day_13/src/lib.rs
+++ day_13/src/lib.rs
@@ -3,6 +3,7 @@ use eyre::Context;
 use itertools::{iproduct, Itertools};
 use regex::Regex;
 use std::str::FromStr;
+use rayon::prelude::*;
 
 #[derive(Debug, Clone)]
 struct Point {
@@ -90,56 +91,19 @@ impl FromStr for Puzzle {
 
 impl Puzzle {
     pub fn part_1(&self) -> i64 {
-        let kind_of_primes = [
-            0, 1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73,
-            79, 83, 89, 97,
-        ];
-
-        let prime_combinations = iproduct!(
-            kind_of_primes.clone().into_iter(),
-            kind_of_primes.clone().into_iter()
-        )
-        .collect::<Vec<_>>();
-
-        let variations = iproduct!(
-            (0..101).into_iter(),
-            (0..101).into_iter()
-        ).collect::<Vec<_>>();
-
-        let increment = 10000000000000_i64;
-        let mut machines = self.machines.clone();
-
-        machines.iter_mut().for_each(|mut machine| {
-            machine.prize.x += increment;
-            machine.prize.y += increment;
-        });
-
-        machines
-            .iter()
+        self.machines
+            .par_iter()
             .filter_map(|machine| {
-                dbg!(&machine);
-                let prize_x_by_b = machine.prize.x / machine.b.point.x; // 381
-                let prize_y_by_b = machine.prize.y / machine.b.point.y; // 80
-
-                let ceiling = max(prize_x_by_b as i64, prize_y_by_b as i64);
-
-                let mut zero_to_two_patterns = (0..ceiling)
+                let mut zero_to_two_patterns = (0..100)
                     .rev()
                     .filter_map(|b_count| {
-                        let prize_without_x = machine.prize.x - b_count as i64 * machine.b.point.x;
-                        let prize_without_y = machine.prize.y - b_count as i64 * machine.b.point.y;
+                        let prize_without_x = machine.prize.x - b_count * machine.b.point.x;
+                        let prize_without_y = machine.prize.y - b_count * machine.b.point.y;
 
                         match prize_without_x % machine.a.point.x == 0_i64 && prize_without_y % machine.a.point.y == 0_i64 {
                             true => {
                                 let a_count_x = prize_without_x / machine.a.point.x;
                                 let a_count_y = prize_without_y / machine.a.point.y;
-
-                                // dbg!(&a_count_x, &a_count_y, &b_count, &machine.a.point, &machine.b.point);
-
-                                // match a_count_x == a_count_y {
-                                //     true => {Some((a_count_x, b_count))}
-                                //     false => {None}
-                                // }
 
                                 Some((a_count_x, a_count_y, b_count))
                             }
@@ -151,7 +115,6 @@ impl Puzzle {
                 match zero_to_two_patterns.next() {
                     Some(first) => match zero_to_two_patterns.next() {
                         Some(second) => {
-                            dbg!(first, second);
                             let mut costs : Vec<i64> = Vec::new();
                             let a_x_diff = second.0 - first.0;
                             let a_y_diff = second.1 - first.1;
@@ -163,12 +126,7 @@ impl Puzzle {
                             let mut b = first.2;
 
                             while b >= 0{
-                                // dbg!(&a_x, &a_y, &b);
-
                                 if a_x == a_y{
-                                    // dbg!("hello", &a_x, &b);
-                                    dbg!("Cost");
-
                                     costs.push(machine.a.cost * a_x + machine.b.cost * b)
                                 }
 
@@ -192,45 +150,9 @@ impl Puzzle {
                 }
             })
             .sum()
-
-        // self
-        //     .machines
-        //     .iter()
-        //     .filter_map(|machine| {
-        //         let prize_x_by_b = machine.prize.x / machine.b.point.x; // 381
-        //         let prize_y_by_b = machine.prize.y / machine.b.point.y; // 80
-        //
-        //         let ceiling = min(min(prize_x_by_b as i64, prize_y_by_b as i64), 100);
-        //
-        //         (0..ceiling)
-        //             .rev()
-        //             .filter_map(|b_count| {
-        //                 let prize_without_x = machine.prize.x - b_count as i64 * machine.b.point.x;
-        //                 let prize_without_y = machine.prize.y - b_count as i64 * machine.b.point.y;
-        //
-        //                 match prize_without_x % machine.a.point.x == 0_i64 && prize_without_y % machine.a.point.y == 0_i64 {
-        //                     true => {
-        //                         let a_count_x = prize_without_x / machine.a.point.x;
-        //                         let a_count_y = prize_without_y / machine.a.point.y;
-        //
-        //                         dbg!(&a_count_x, &a_count_y, &b_count, &machine.a.point, &machine.b.point);
-        //
-        //                         match a_count_x == a_count_y {
-        //                             true => {Some((a_count_x, b_count))}
-        //                             false => {None}
-        //                         }
-        //                     }
-        //                     false => {None}
-        //                 }
-        //             })
-        //             .filter_map(|(a_count, b_count)| {
-        //                 Some((a_count.round() as i64 * machine.a.cost + b_count * machine.b.cost) as u32)
-        //             }).min()
-        //     })
-        //     .sum()
     }
 
-    pub fn part_2(&self) -> u32 {
+    pub fn part_2(&self) -> i64 {
         let increment = 10000000000000_i64;
         let mut machines = self.machines.clone();
 
@@ -240,44 +162,67 @@ impl Puzzle {
         });
 
         machines
-            .iter()
+            .par_iter()
             .filter_map(|machine| {
-                let prize_x_by_b = machine.prize.x / machine.b.point.x;
-                let prize_y_by_b = machine.prize.y / machine.b.point.y;
+                let prize_x_by_b = machine.prize.x / machine.b.point.x; // 381
+                let prize_y_by_b = machine.prize.y / machine.b.point.y; // 80
 
-                let ceiling = min(prize_x_by_b, prize_y_by_b);
-                // dbg!(&ceiling);
+                let ceiling = max(prize_x_by_b as i64, prize_y_by_b as i64);
 
-                (0..ceiling)
+                let mut zero_to_two_patterns = (0..ceiling)
                     .rev()
                     .filter_map(|b_count| {
-                        let prize_without_x = machine.prize.x - b_count as i64 * machine.b.point.x;
-                        let prize_without_y = machine.prize.y - b_count as i64 * machine.b.point.y;
+                        let prize_without_x = machine.prize.x - b_count * machine.b.point.x;
+                        let prize_without_y = machine.prize.y - b_count * machine.b.point.y;
 
                         match prize_without_x % machine.a.point.x == 0_i64 && prize_without_y % machine.a.point.y == 0_i64 {
                             true => {
                                 let a_count_x = prize_without_x / machine.a.point.x;
                                 let a_count_y = prize_without_y / machine.a.point.y;
-                                
-                                let b_something = a_count_x as i64;
 
-                                // dbg!(&a_count_x, &a_count_y, &b_count, &machine.a.point, &machine.b.point);
-
-                                match a_count_x == a_count_y {
-                                    true => {
-                                        // dbg!("189");
-
-                                        Some((a_count_x, b_count))
-                                    }
-                                    false => {None}
-                                }
+                                Some((a_count_x, a_count_y, b_count))
                             }
-                            false => {None}
+                            false => { None }
                         }
                     })
-                    .filter_map(|(a_count, b_count)| {
-                        Some((a_count * machine.a.cost + b_count * machine.b.cost) as u32)
-                    }).min()
+                    .take(2);
+
+                match zero_to_two_patterns.next() {
+                    Some(first) => match zero_to_two_patterns.next() {
+                        Some(second) => {
+                            let mut costs: Vec<i64> = Vec::new();
+                            let a_x_diff = second.0 - first.0;
+                            let a_y_diff = second.1 - first.1;
+                            let b_diff = first.2 - second.2;
+
+
+                            let mut a_x = first.0;
+                            let mut a_y = first.1;
+                            let mut b = first.2;
+
+                            while b >= 0 {
+                                if a_x == a_y {
+                                    costs.push(machine.a.cost * a_x + machine.b.cost * b)
+                                }
+
+                                a_x += a_x_diff;
+                                a_y += a_y_diff;
+                                b -= b_diff
+                            }
+
+                            costs.into_iter().min()
+                        }
+                        None => {
+                            match first.0 == first.1 {
+                                true => {
+                                    Some(machine.a.cost * first.0 + machine.b.cost * first.2)
+                                }
+                                false => { None }
+                            }
+                        }
+                    },
+                    None => None,
+                }
             })
             .sum()
     }
@@ -315,6 +260,7 @@ Prize: X=18641, Y=10279";
     }
 
     #[test]
+    #[ignore] // this is currently way too unoptimized
     fn should_solve_part_2() {
         // Given
         let puzzle = Puzzle::from_str(INPUT).unwrap();
