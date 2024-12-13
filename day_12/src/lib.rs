9f7aafa931c0b53cf5782bcd4e862f8ba8db5dc4
gutsdream <angus.hankinson@gmail.com>
2024-12-13 18:19:33 +1300

    4ms/5ms -> 1.7/1.9ms with rayon impl

--- day_12/src/lib.rs
+++ day_12/src/lib.rs
@@ -2,10 +2,12 @@ mod direction;
 mod region;
 
 use crate::direction::STRAIGHT;
+use crate::region::Area;
 use direction::Point;
 use std::collections::{HashMap, HashSet};
 use std::str::FromStr;
-use crate::region::Area;
+use std::sync::{Arc, Mutex};
+use rayon::prelude::*;
 
 pub struct Puzzle {
     regions: Vec<HashSet<Point>>,
@@ -15,67 +17,67 @@ impl FromStr for Puzzle {
     type Err = ();
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let mut grid: HashMap<Point, char> = input
-            .lines()
-            .enumerate()
-            .map(|(y, line)| {
-                line.chars()
-                    .enumerate()
-                    .map(|(x, c)| (Point { x, y }, c))
-                    .collect::<Vec<_>>()
-            })
-            .flatten()
-            .collect();
+        let mut regions = Arc::new(Mutex::new(Vec::new()));
 
-        let mut regions : Vec<HashSet<Point>> = Vec::new();
+        let mut grid: HashMap<char, HashSet<Point>> = HashMap::new();
+
+        input.lines().enumerate().for_each(|(y, line)| {
+            line.chars().enumerate().for_each(|(x, c)| {
+                grid.entry(c).or_default().insert(Point { x, y });
+            })
+        });
 
         fn build_region_points(
-            grid: &mut HashMap<Point, char>,
-            point: Point,
             points: &mut HashSet<Point>,
+            point: Point,
+            contacting_points: &mut HashSet<Point>,
         ) {
-            if let Some(char) = grid.remove(&point) {
-
+            if points.remove(&point) {
                 STRAIGHT
                     .iter()
                     .filter_map(|direction| point.gen_in_direction(direction))
                     .for_each(|point| {
-                        if let Some(c) = grid.get(&point) {
-                            if c == &char {
-                                build_region_points(grid, point, points)
-                            }
+                        if points.contains(&point) {
+                                build_region_points(points, point, contacting_points)
                         }
                     });
 
-                points.insert(point);
+                contacting_points.insert(point);
             }
         }
 
-        input.lines().enumerate().for_each(|(y, line)| {
-            line.chars()
-                .enumerate()
-                .for_each( |(x, c)| {
-                    let mut points = HashSet::new();
-                    build_region_points(&mut grid,Point{x, y}, &mut points);
-                    if points.len() > 0 {
-                        regions.push(points);
+        grid.into_par_iter().for_each(|(char, mut points)| {
+            points.clone().into_iter().for_each(|point| {
+                if points.contains(&point) {
+                    let mut contacting_points = HashSet::new();
+                    build_region_points(&mut points, point.clone(), &mut contacting_points);
+                    if contacting_points.len() > 0 {
+                        let regions = regions.clone();
+                        if let Some(mut regions) = regions.lock().ok() {
+                            regions.push(contacting_points);
+                        };
                     }
-                })
+                }
+            })
         });
 
-        Ok(Puzzle {
-            regions
-        })
+        Ok(Puzzle { regions: Arc::try_unwrap(regions).unwrap().into_inner().unwrap() })
     }
 }
 
 impl Puzzle {
     pub fn part_1(&self) -> u32 {
-        self.regions.iter().map(|region| region.area() * region.perimeter()).sum()
+        self.regions
+            .par_iter()
+            .map(|region| region.area() * region.perimeter())
+            .sum()
     }
 
     pub fn part_2(&self) -> u32 {
-        self.regions.iter().map(|region| region.area() * region.sides()).sum()
+        self.regions
+            .par_iter()
+            .map(|region| region.area() * region.sides())
+            .sum()
     }
 }
 
